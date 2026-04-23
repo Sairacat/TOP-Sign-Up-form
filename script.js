1d81b7af34f83308216c705b1ade630eda536bd2
Sairacat <201769jzy@gmail.com>
2026-04-23 16:39:20 +0800

    Add some code to check whether the value entered into the confirmation input has matched with the password entered before If it is matched, a ✅sign will appear. if not, a ❌sign will appear.

--- script.js
+++ script.js
@@ -1,8 +1,75 @@
 const toggleButton = document.querySelector('.toggle-btn')
 const togglePanel = document.querySelector('.rightside-wrapper')
 const logWrapper = document.querySelector('.log-wrapper')
+const errorMessage1 = document.querySelector('.error-message1')
+const errorMessage2 = document.querySelector('.error-message2')
+const passwordConfirm = document.querySelector('#password-confirm')
+const password = document.querySelector('#password')
+const checkmark1 = document.querySelector('.checkmark1')
+const checkmark2 = document.querySelector('.checkmark2')
+
 
 toggleButton.addEventListener('click', function(e) {
     togglePanel.classList.toggle('collapse')
     logWrapper.classList.toggle('log-move')
 })
+
+password.addEventListener('blur', function(e) {
+    if(password.value === '') {
+        errorMessage1.textContent = 'Required'
+        return
+    }
+})
+
+password.addEventListener('focus', function(e) {
+    errorMessage1.textContent = ''
+})
+
+
+passwordConfirm.addEventListener('blur', function(e){
+    if(passwordConfirm.value === '') {
+        errorMessage2.textContent = 'Required'
+        return
+    }
+    if(password.value === passwordConfirm.value) {
+        errorMessage2.textContent = ''
+    }else {
+        errorMessage2.textContent = 'Please enter the same password'
+        passwordConfirm.classList.add('wrong')
+    }
+})
+
+passwordConfirm.addEventListener('focus', function(e) {
+    errorMessage2.textContent = ''
+})
+
+password.addEventListener('input', function(e) {
+    if(password.value === '') {
+        checkmark1.textContent = ''
+        return
+    }
+    if(!password.checkValidity()) {
+        checkmark1.textContent = '❌'
+        errorMessage1.textContent = 'Password should be at least 8 digit with a Captial letter and multiple numbers'
+        password.classList.add('touched')
+    }else {
+        checkmark1 = '✅'
+        errorMessage1.textContent = ''
+        password.classList.remove('touched')
+    }
+})
+
+passwordConfirm.addEventListener('input', function(e) {
+    if(passwordConfirm.value === '') {
+        checkmark2.textContent = ''
+        return
+    } 
+    if(password.value === passwordConfirm.value) {
+        checkmark2.textContent = '✅'
+        passwordConfirm.classList.remove('touched')
+    }else {
+        checkmark2.textContent = '❌'
+        passwordConfirm.classList.add('touched')
+    }
+})
+
